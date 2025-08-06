405db68e6d91932226c053180924d7e29219aecd
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-08-06 09:23:37 -0300

    feat: add category entity, repository and service

---
+++ src/main/java/com/movieflix/entity/Category.java
@@ -0,0 +1,18 @@
+package com.movieflix.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private final String id;
+
+    @Column(length = 100, nullable = false)
+    private final String name;
+}
---
+++ src/main/java/com/movieflix/service/CategoryService.java
@@ -0,0 +1,7 @@
+package com.movieflix.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+}
