bcea8545c14df09d9c8f0d28a8f264a3a67d5ccb
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-08-09 09:38:07 -0300

    feat: add route to create category

--- src/main/java/com/movieflix/controller/CategoryController.java
+++ src/main/java/com/movieflix/controller/CategoryController.java
@@ -5,10 +5,9 @@ import com.movieflix.service.CategoryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +18,16 @@ public class CategoryController {
 
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
+    }
+
+    @Operation(summary = "Create register of category")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Category was successfully registered")
+    })
+    @PostMapping
+    public ResponseEntity<Void> createFoodItem(@RequestBody Category category) {
+        categoryService.create(category);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @Operation(summary = "Get all categories")
