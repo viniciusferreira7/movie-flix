94cb7422a81fbcbbaf1a9c92d65c44a0c00aa2de
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-08-09 09:09:02 -0300

    feat: add method to find all categories on category service

--- src/main/java/com/movieflix/service/CategoryService.java
+++ src/main/java/com/movieflix/service/CategoryService.java
@@ -1,7 +1,20 @@
 package com.movieflix.service;
 
+import com.movieflix.entity.Category;
+import com.movieflix.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> findAll(){
+        return categoryRepository.findAll();
+    }
 }
