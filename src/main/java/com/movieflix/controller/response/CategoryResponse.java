1cd80f7b7d1f21099d7343537479ae51db8fda37
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-08-13 09:40:02 -0300

    refactor: create DTO and mappers to category

--- src/main/java/com/movieflix/controller/response/CategoryResponse.java
+++ src/main/java/com/movieflix/controller/response/CategoryResponse.java
@@ -1,4 +1,7 @@
 package com.movieflix.controller.response;
 
+import lombok.Builder;
+
+@Builder
 public record CategoryResponse(Long id, String name) {
 }
--- src/main/java/com/movieflix/entity/Category.java
+++ src/main/java/com/movieflix/entity/Category.java
@@ -1,9 +1,11 @@
 package com.movieflix.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @Entity
@@ -11,7 +13,7 @@ import lombok.Setter;
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Long id;
 
     @Column(length = 100, nullable = false, unique = true)
     private String name;
