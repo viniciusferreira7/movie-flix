90ef804647e4057c5552367e53dc5ebce38b4a43
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-09-24 09:56:08 -0300

    feat: add encode of password

--- src/main/java/com/movieflix/service/UserService.java
+++ src/main/java/com/movieflix/service/UserService.java
@@ -3,14 +3,18 @@ package com.movieflix.service;
 import com.movieflix.entity.User;
 import com.movieflix.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public User register(User user){
+        String password = user.getPassword();
+        user.setPassword(passwordEncoder.encode(password));
         return userRepository.save(user);
     }
 }
